c797c7dc57fc1a27bf62ac847328c3b8cce0d880
Vegar Lee Berentsen <designr.pros>
2025-04-07 14:53:07 +0200

    MetaTag Sandvika platemesse - Høl i cven

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,22 @@ interface RootLayoutProps {
   children: ReactNode;
 }
 
+export const metadata = {
+  title: "Sandvika Platemesse",
+  description: "Høl i Cvén - Studio 51 - Folkebadet - Ungdom og fritid Bærum Kommune",
+  keywords: "Sandvika Platemesse, Høl i Cvén, Studio 51, Folkebadet, Ungdom og fritid, Bærum Kommune",
+};
+
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="title" content="Sandvika Platemesse" />
+        <meta name="description" content="Høl i Cvén - Studio 51 - Folkebadet - Ungdom og fritid Bærum Kommune" />
+        <meta name="keywords" content="Sandvika Platemesse, Høl i Cvén, Studio 51, Folkebadet, Ungdom og fritid, Bærum Kommune" />
+      </head>
       <body className={`${bebas.className} antialiased`}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
